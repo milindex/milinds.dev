54004a2e1bf99af88d3a6db5336e6ac94e993d8d
Milind <40736631+milindex@users.noreply.github.com>
2026-06-13 16:00:30 +0530

    feat: upgrade deps to Next.js 16, Tailwind v4, React 19

--- next.config.js
+++ next.config.js
@@ -1,24 +1,20 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')(['@svgr/webpack']);
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 const path = require('path');
 
-module.exports = withPlugins([withTM, withBundleAnalyzer], {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   eslint: {
     dirs: ['.'],
   },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-});
+};
+
+module.exports = nextConfig;
